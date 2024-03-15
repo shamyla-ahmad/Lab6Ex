c3a1e406223ead84c0694ad8c2c57b8e022a84c3
Shamyla Ahmad <ec23130@qmul.ac.uk>
2024-03-15 09:50:53 +0000

    test2

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
+<<<<<<< Updated upstream
         System.out.println("Hello World!");
+=======
+        System.out.println("Hello World 123456!");
+>>>>>>> Stashed changes
     }
 }
